3aed514c03ee937bac9c8c97ffe440e1063ed7df
code-looney <danielvbofficieel@gmail.com>
2024-06-06 23:47:11 +0200

    working on  the cart mobile

--- src/App.jsx
+++ src/App.jsx
@@ -144,7 +144,7 @@ export default function App() {
       <Container className="w-full md:flex md:h-[650px] md:items-center justify-center md:gap-20">
         <Container className="md:w-96 w-full relative md:flex md:flex-col md:justify-between md:h-[520px]">
           <Lightbox src={`public/images/image-product-${image}.jpg`} className="md:rounded-xl w-full h-[400px] object-cover" />
-          <Cart className="absolute outline h-full w-full justify-center items-center flex top-0 md:hidden">cart</Cart>
+          <Cart className="absolute outline h-[95%] w-[97%] justify-center items-center flex inset-0 m-auto bg-white md:hidden">cart</Cart>
           <Container className="hidden md:flex flex-row justify-between gap-4">
             {selectImage && selectImage.map((item, index) => {
               return (
@@ -158,7 +158,7 @@ export default function App() {
               )
             })}
           </Container>
-          <Container className="absolute md:hidden outline h-full justify-between items-center flex top-0 w-full px-3 oultine">
+          <Container className="absolute md:hidden outline h-full justify-between items-center flex top-0 w-full px-3">
             <Container className="bg-white w-10 h-10 flex items-center justify-center rounded-full md:hidden">
               <ButtonIcon disabled={image === 1 ? true : false} onClick={hamdlePrevImageClick} className="bg-white w-3 h-4 flex items-center justify-center rounded-full" classnameiconbtn="w-full h-full rounded-full flex justify-center items-center" src="public/images/icon-previous.svg" />
             </Container>
@@ -180,7 +180,8 @@ export default function App() {
               </Container>
               <Container className="flex flex-col">
               {price && price.map(item => {
-                 // dynamic id plaatsen / leren
+                 // dynamic id plaatsen / leren !!!
+
                return (
                   <Container key={item.id} className="flex flex-col">
                     {/* { console.log(crypto.randomUUID() )} */}

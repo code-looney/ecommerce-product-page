610e26da0a5abf4eed24083514af2256ebbe70c8
code-looney <codelooney@Daneels-MBP.home>
2024-06-19 18:53:58 +0200

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -189,6 +189,14 @@ export default function App() {
         <Container className="md:w-96 w-full relative md:flex md:flex-col md:justify-between md:h-[500px] md:p-0">
           <Lightbox onClick={() => setToggleLightBoxFullSize('block')} src={`/images/image-product-${image}.jpg`} className="md:rounded-xl w-full h-[330px] md:h-[380px] object-cover" />
           {/* check if the cart w/h dimensions are correct */}
+
+
+        {/* morgen gaan we oplossen hoe we de product in the basket alleen count wanneer je op add to basket clicked en niet wanner een product in de basket is en we de count vehogen niet auto aanpast
+        want op dit moment verbergen we alleen de procut en telt het product verborgen op!!! */}
+
+
+
+
           <CartBasket className={`${removeCartBasket} absolute shadow-2xl z-20 h-[85%] md:h-[50%] w-[95%] justify-start flex-col flex top-[7%] left-[2.5%] m-auto bg-white rounded-lg translate-y-[-4%] md:translate-y-[-50%] md:translate-x-[200%]`}>
             <Container className={`flex-col flex-1 flex`}>
               <Container className="w-full flex flex-col justify-center gap-5 pt-5">
@@ -199,8 +207,11 @@ export default function App() {
                   <Rule className="w-full" />
               </Container>
               <Container className="h-full w-full flex flex-col justify-center items-center px-6 gap-7"><Sub className={`${count === 0 ? "block" : "hidden"}`}>Your cart is empty.</Sub>
+              {/* De emtpy para verdwijnt wanneer de count verhoogd beven de 0? Dat willen we moeten ervoor zorgen dat de empty para alleen verdwijnt wanneer we een product toevoegen! */}
               {/* refactor the  empty */}
-              <Container className={`flex gap-4 items-center w-full ${count >= 1 ? "block" : "hidden"}`}> {/* This is the basket product section */}
+              {/* This is the basket product section */}
+              <Container className={`flex gap-4 items-center w-full ${iconCounter >= 1 ? "block" : iconCounter === 0 ? "hidden" : "hidden"}`}> {/* pass aan!!! Wanneer de count op 0 is verwijdered het product niet? 
+              zorg er dus voor dat wanneer de count op 0 komt te staan het product verwijderen en de empty para tevoorschijn komt!*/}
                 <Thumbnail className="w-16 md:w-12 rounded-md" src="/images/image-product-1-thumbnail.jpg" />
                 <Container><Sub className="text-darkGrayish">Fall Limited Edition Sneakers</Sub>
                 {price && price.map(item => { {/* This is the cartbasket product details */}
@@ -216,11 +227,14 @@ export default function App() {
                 {/* de delete button moet de product verwijderen zelf als de count hoger is dan 1 
                 dus zorg er voor de de gebuiker met een edit button de count can aanpassen*/}
               </Container>
-              <Container className={`w-full flex justify-center ${count >= 1 ? "block" : "hidden"}`}><Button  onClick={handleClick} className="bg-orange w-full py-4 md:py-3 rounded-lg text-white">Checkout</Button></Container>
+              <Container className={`w-full flex justify-center ${iconCounter >= 1 ? "block" : "hidden"}`}><Button  onClick={handleClick} className="bg-orange w-full py-4 md:py-3 rounded-lg text-white">Checkout</Button></Container>
               {/* refactor the checkout button */}
               </Container>
             </Container>
           </CartBasket>
+
+
+
           <Container className="hidden md:flex flex-row justify-between gap-4">
             {selectImage && selectImage.map((item, index) => {
               return (
@@ -233,7 +247,6 @@ export default function App() {
                   </Container>
               )
             })}
-            {/* test */}
           </Container>
           <Container className="absolute z-10 md:hidden h-full justify-between items-center flex top-0 w-full px-3">
             <Container className="bg-white w-10 h-10 flex items-center justify-center rounded-full md:hidden">

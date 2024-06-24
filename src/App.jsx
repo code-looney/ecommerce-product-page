bcfbea46a8032f97d4a6cdefc98eb2c2467c1888
code-looney <daneelvb1001@gmail.com>
2024-06-24 20:41:16 +0200

    mod refactored comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,17 @@
 import React, { useEffect, useState } from "react"
 import Container from "./components/Container"
 import Header from "./components/Header"
-import Lightbox from "./components/Lightbox"
 import Rule from "./components/Rule"
-import ButtonIcon from "./components/ButtonIcon"
-import Title from "./components/Title"
-import Sub from "./components/Sub"
-import Paragraph from "./components/Paragraph"
-import BasePrice from "./components/BasePrice"
-import Sale from "./components/Sale"
-import FullPrice from "./components/FullPrice"
-import Counter from "./components/Counter"
-import Icon from "./components/Icon"
-import Button from "./components/Button"
-import Thumbnail from "./components/Thumbnail"
-import CartBasket from "./components/CartBasket"
 import LightBoxFullSize from "./components/LightBoxFullSize"
 import {loadStripe} from "@stripe/stripe-js";
-import ImageThumbnailSelector from "./ImageThumbnailSelector"
-import ImageNavigationControls from "./ImageNavigationControls"
-import ProductPriceDetails from "./ProductPriceDetails"
 import LightboxClose from "./LightboxClose"
+import LightboxContainer from "./LightboxContainer"
+import ProductDisplay from "./ProductDisplay"
 
 
 export default function App() {
   const [count, setCount] = useState(0); 
   const [price, setPrice] = useState(null)
-  const [amount, setAmount] = useState(null)
-
-  const [discount, setDiscount] = useState(50)
-  const [nextImage, setNextImage] = useState(null)
-  const [prevImage, setPrevImage] = useState(null)
-  const [disable, setDisable] = useState(false)
   const [image, setImage] = useState(1)
   const [selectImage, setSelectImage] = useState(null)
   const [removeCartBasket, setRemoveCartBasket] = useState('hidden');
@@ -182,68 +162,34 @@ export default function App() {
   return (
     <>
     <Container className="h-screen pb-[800px] md:pb-0 overflow-hidden">
-      <Header iconCounterHidden={iconCounterHidden} iconCounter={iconCounter} count={count} removeCartBasket={removeCartBasket} setRemoveCartBasket={setRemoveCartBasket} />
+      <Header 
+      iconCounterHidden={iconCounterHidden} 
+      iconCounter={iconCounter} 
+      count={count} 
+      removeCartBasket={removeCartBasket} 
+      setRemoveCartBasket={setRemoveCartBasket} />
       <Container className="hidden md:flex justify-center w-full px-[240px]">
         <Rule className="md:w-full"/>
       </Container>
-      <Container className="w-full md:flex md:h-[650px] md:items-center justify-center md:gap-20">
-        <Container className="md:w-96 w-full relative md:flex md:flex-col md:justify-between md:h-[500px] md:p-0">
-          <Lightbox onClick={() => setToggleLightBoxFullSize('block')} src={`/images/image-product-${image}.jpg`} className="md:rounded-xl w-full h-[330px] md:h-[380px] object-cover" />
-          {/* check if the cart w/h dimensions are correct */}
-
-          {/* morgen gaan we oplossen hoe we de product in the basket alleen count wanneer je op add to basket clicked en niet wanner een product in de basket is en we de count vehogen niet auto aanpast
-          want op dit moment verbergen we alleen de procut en telt het product verborgen op!!! */}
-
-
-          <CartBasket count={count} iconCounter={iconCounter} price={price} onDeleteClick={handleDeleteClick} onHandleClick={handleClick} className={`${removeCartBasket} absolute shadow-2xl z-20 h-[85%] md:h-[50%] w-[95%] justify-start flex-col flex top-[7%] left-[2.5%] m-auto bg-white rounded-lg translate-y-[-4%] md:translate-y-[-50%] md:translate-x-[200%]`}></CartBasket>
-          <ImageThumbnailSelector onSelectedImage={handleSelectedImage} selectImage={selectImage} borderColor1={borderColor1} borderColor2={borderColor2} borderColor3={borderColor3} borderColor4={borderColor4} opacity1={opacity1} opacity2={opacity2} opacity3={opacity3} opacity4={opacity4} />
-          <ImageNavigationControls image={image} onPrevImageClick={hamdlePrevImageClick} onHandleClick={hamdleNextImageClick} />
-
-        </Container>
-
-        <ProductPriceDetails count={count} price={price} onAddToCart={handleAddToCart} onAddProduct={handleAddProduct} onSubProduct={handleSubProduct} />
-        
-      </Container>
+      <ProductDisplay />
     </Container>
-    <LightBoxFullSize className={`${toggleLightBoxFullSize} absolute top-0 flex justify-center w-full h-full items-center bg-black bg-opacity-70 z-50`}>
-          {/* begin */}
-          <LightboxClose setToggleLightBoxFullSize={setToggleLightBoxFullSize} />
-          {/* end */}
-        <Container className="md:w-[450px] relative md:flex md:flex-col md:justify-between md:h-[550px] gap-10 translate-y-10">          
-          {/* check de thumbnails positie nogmaals (hij valt uit de div)*/}
-          <Lightbox src={`/images/image-product-${image}.jpg`} className="md:rounded-xl w-full md:h-full object-cover" />
-          {/* check if the cart w/h dimensions are correct */}
-          <Container className="md:flex flex-row justify-center gap-6 relative z-20">
-            {selectImage && selectImage.map((item, index) => {
-              return (
-                <Container key={item.id} className="flex items-center translate-y-[-6px]">
-                  {/* dit zijn de thumbnail border colors...*/}
-                  <Button className={`rounded-xl bg-white w-[70px] ${item.id === 1 ? borderColor1 : ""} ${item.id === 2 ? borderColor2 : ""} ${item.id === 3 ? borderColor3 : ""} ${item.id === 4 ? borderColor4 : ""}`} onClick={() => handleSelectedImage(item.id, index)}>
-                                      {/* maak de tweede lightbox/thumbnails onafhankelijk...*/}
-
-                  {/* dit zijn de thumbnail opacities...*/}
-                        <Thumbnail className={`rounded-xl hover:opacity-50 ${item.id === 1 ? opacity1 : ""} ${item.id === 2 ? opacity2 : ""} ${item.id === 3 ? opacity3 : ""} ${item.id === 4 ? opacity4 : ""}`} src={`/images/image-product-${item.image}-thumbnail.jpg`} />
-                  </Button>
-                  </Container>
-              )
-            })}
-          </Container>
-          <Container className="absolute z-10  h-[80%] n justify-between items-center flex w-full px-3">
-                  {/* refactor de next en prev zodat het accuraat in het midden komt te staan */}
-
-            <Container className="bg-white w-10 h-10 flex items-center justify-center rounded-full translate-x-[-30px] ">
-                  {/* refactor de next en prev zodat het accuraat in het midden komt te staan */}
-
-              <ButtonIcon disabled={image === 1 ? true : false} onClick={hamdlePrevImageClick} className="bg-white w-3 h-4 flex items-center justify-center rounded-full" classnameiconbtn="w-full h-full rounded-full flex justify-center items-center" src="/images/icon-previous.svg" />
-            </Container>
-            <Container className="bg-white w-10 h-10 flex items-center justify-center rounded-full  translate-x-[30px] ">
-                  {/* refactor de next en prev zodat het accuraat in het midden komt te staan */}
-
-              <ButtonIcon disabled={image === 4 ? true : false} onClick={hamdleNextImageClick} className="bg-white w-3 h-4 flex items-center justify-center rounded-full" classnameiconbtn="w-full h-full rounded-full flex justify-center items-center" src="/images/icon-next.svg" />
-            </Container>
-          </Container>
-        </Container>
-    </LightBoxFullSize>
-  </>
+        <LightBoxFullSize className={`${toggleLightBoxFullSize} absolute top-0 flex justify-center w-full h-full items-center bg-black bg-opacity-70 z-50`}>
+        <LightboxClose setToggleLightBoxFullSize={setToggleLightBoxFullSize} />
+        <LightboxContainer 
+          borderColor1={borderColor1} 
+          borderColor2={borderColor2} 
+          borderColor3={borderColor3} 
+          borderColor4={borderColor4} 
+          opacity1={opacity1} 
+          opacity2={opacity2} 
+          opacity3={opacity3} 
+          opacity4={opacity4} 
+          selectImage={selectImage} 
+          image={image} 
+          setImage={setImage} 
+          onPrevImageClick={hamdlePrevImageClick} 
+          onNextImageClick={hamdleNextImageClick} />
+        </LightBoxFullSize>
+    </>
   )
 }
--- src/LightboxContainer.jsx
+++ src/LightboxContainer.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
+import Container from './components/Container'
+import Lightbox from './components/Lightbox'
+import ButtonIcon from './components/ButtonIcon'
+import Button from './components/Button'
+import Thumbnail from './components/Thumbnail'
 
-const LightboxContainer = () => {
+const LightboxContainer = (props) => {
+  const {image, setImage, selectImage, borderColor1, borderColor2, borderColor3, borderColor4, 
+    opacity1, opacity2, opacity3, opacity4} = props
+
   return (
-    <div>LightboxContainer</div>
+    <Container className="md:w-[450px] relative md:flex md:flex-col md:justify-between md:h-[550px] gap-10 translate-y-10">          
+    {/* check de thumbnails positie nogmaals (hij valt uit de div)*/}
+    <Lightbox src={`public/images/image-product-${image}.jpg`} className="md:rounded-xl w-full md:h-full object-cover" />
+    {/* check if the cart w/h dimensions are correct */}
+    <Container className="md:flex flex-row justify-center gap-6 relative z-20">
+      {selectImage && selectImage.map((item, index) => {
+        return (
+          <Container key={item.id} className="flex items-center translate-y-[-6px]">
+            {/* dit zijn de thumbnail border colors...*/}
+            <Button className={`rounded-xl bg-white w-[70px] ${item.id === 1 ? borderColor1 : ""} ${item.id === 2 ? borderColor2 : ""} ${item.id === 3 ? borderColor3 : ""} ${item.id === 4 ? borderColor4 : ""}`} onClick={() => handleSelectedImage(item.id, index)}>
+              {/* maak de tweede lightbox/thumbnails onafhankelijk...*/}
+
+            {/* dit zijn de thumbnail opacities...*/}
+                  <Thumbnail className={`rounded-xl hover:opacity-50 ${item.id === 1 ? opacity1 : ""} ${item.id === 2 ? opacity2 : ""} ${item.id === 3 ? opacity3 : ""} ${item.id === 4 ? opacity4 : ""}`} src={`/images/image-product-${item.image}-thumbnail.jpg`} />
+            </Button>
+            </Container>
+        )
+      })}
+    </Container>
+    <Container className="absolute z-10  h-[80%] n justify-between items-center flex w-full px-3">
+            {/* refactor de next en prev zodat het accuraat in het midden komt te staan */}
+
+      <Container className="bg-white w-10 h-10 flex items-center justify-center rounded-full translate-x-[-30px] ">
+            {/* refactor de next en prev zodat het accuraat in het midden komt te staan */}
+
+        <ButtonIcon disabled={image === 1 ? true : false} onClick={() => props.onPrevImageClick()} className="bg-white w-3 h-4 flex items-center justify-center rounded-full" classnameiconbtn="w-full h-full rounded-full flex justify-center items-center" src="/images/icon-previous.svg" />
+      </Container>
+      <Container className="bg-white w-10 h-10 flex items-center justify-center rounded-full  translate-x-[30px] ">
+            {/* refactor de next en prev zodat het accuraat in het midden komt te staan */}
+        <ButtonIcon disabled={image === 4 ? true : false} onClick={() => props.onNextImageClick()} className="bg-white w-3 h-4 flex items-center justify-center rounded-full" classnameiconbtn="w-full h-full rounded-full flex justify-center items-center" src="/images/icon-next.svg" />
+      </Container>
+    </Container>
+  </Container>
   )
 }
 
